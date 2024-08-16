4ac73ae7650c979480337b20a191f953ce3f325c
Fernando Soares <165102007+thunderfer81@users.noreply.github.com>
2024-08-16 18:25:36 -0300

    Adicionando pasta da aventura web.
    Renomear e/ou criar um repositório.

---
+++ Teste/script.js
@@ -0,0 +1,12 @@
+
+const avanca = document.querySelectorAll('.btn-proximo');
+
+avanca.forEach(button => {
+    button.addEventListener('click', function(){
+        const atual = document.querySelector('.ativo');
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
+
+        atual.classList.remove('ativo');
+        document.getElementById(proximoPasso).classList.add('ativo');
+    })
+})
